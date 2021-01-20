941e0fc9163e1d68055f65513dce7ec8d71ce93b
Timothy Isenegger <timothy.isenegger@gmail.com>
2021-01-20 14:12:23 +0100

    Initial commit after creating vuejs project

---
+++ random-name-generator/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <header>
+      <div class="container">
+        <div class="title">Random Name Generator</div>
+        <div class="subtitle">with vuejs</div>
+      </div>
+    </header>
+    <main>
+      <div class="container">
+        <name-generator></name-generator>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import NameGenerator from './components/NameGenerator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NameGenerator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.container {
+  width: 100%;
+  max-width: 1110px;
+  padding: 0 15px;
+  margin: 0 auto;
+}
+
+.title {
+  font-size: 2rem;
+}
+
+.subtitle {
+  font-size: 1.5rem;
+}
+</style>
---
+++ random-name-generator/src/components/NameGenerator.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="name-generator">
+        <h2>Select the desired gender</h2>
+        <select id="selectGender">
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+        </select>
+
+        <h2>Select the desired race</h2>
+        <select id="selectRace">
+            <option value="human">Human</option>
+            <option value="elf">Elf</option>
+        </select>
+
+        <div>
+            <button id="buttonGenerate" @click="generateName" class="btn btn-primary">Generate now</button>
+        </div>
+        <h2>Your generated name is</h2>
+        <span class="firstname">{{ firstname }}</span>&nbsp;<span class="lastname">{{ lastname }}</span> 
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NameGenerator',
+    data () {
+        return {
+            firstname: '',
+            lastname: '',
+            firstnames: [
+                'Tim',
+                'Lukas',
+            ],
+            lastnames: [
+                'Isenegger',
+                'Fischer',
+            ]
+        }
+    },
+    computed: {
+        currentGender() {
+            return document.getElementById('selectGender').value;
+        },
+        currentRace() {
+            return document.getElementById('selectRace').value;
+        }
+    },
+    methods: {
+        generateName() {
+            this.firstname = this.firstnames[0];
+            this.lastname = this.lastnames[1];
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .name-generator {
+        text-align: left;
+    }
+</style>
+
