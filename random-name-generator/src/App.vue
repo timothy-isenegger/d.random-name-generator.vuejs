b168998c2dfd237ce0a4c2d9e2cff18f9398c7fd
Timothy <timmy.isenegger@gmail.com>
2021-01-23 11:03:29 +0100

    [TASK] Transform solution from index to vue application, add styles

--- random-name-generator/src/App.vue
+++ random-name-generator/src/App.vue
@@ -26,13 +26,17 @@ export default {
 </script>
 
 <style>
+*, *::before, *::after {
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px 0;
 }
 
 .container {
